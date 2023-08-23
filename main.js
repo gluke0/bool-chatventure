3713f7b8ce48a9c09315eff7dbb04edccdf2a56f
gluke0 <milanesigianluca@gmail.com>
2023-08-23 15:31:42 +0200

    fixed template in html for the dnone

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const API_BASE_URL = 'https://api.openai.com/v1';
+const API_KEY = '';
+const GPT_MODEL = 'gpt-3.5-turbo';
+
+const loader = document.querySelector('.loader');
+const genre = document.querySelectorAll('.genre');
+const placeholder = document.querySelector('#placeholder');
+const stage = document.querySelector('#stage');
+const gameover = document.querySelector('#gameover');
+
